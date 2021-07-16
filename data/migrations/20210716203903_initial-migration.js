54aedc21c841cd00515217db8390a17af22e4456
Devon Osei <oseidevon@gmail.com>
2021-07-16 17:26:30 -0400

    migrating

--- data/migrations/20210716203903_initial-migration.js
+++ data/migrations/20210716203903_initial-migration.js
@@ -2,24 +2,46 @@
 exports.up = async function(knex) {
   await knex.schema
   .createTable('projects', table => {
-    table.increments("project_id")
-    table.string('project_name')
+    table.increments('project_id')
+    table.string('project_name').notNullable
+    table.string('project_description')
+    table.integer('project_completed')
   })
 
   .createTable('resources', table => {
-    table.increments("resource_id")
-    table.string('resources_name')
+    table.increments('resource_id')
+    table.string('resources_name').notNullable
+    table.string('resource_description')
   })
   .createTable('tasks', table => {
-    table.increments("task_id")
-    table.string('task_description')
+    table.increments('task_id')
+    table.string('task_description').notNullable
+    table.string('task_notes')
+    table.integer('task_completed')
+    // project-id needs to be connected
+    table.integer('project_id')
+
   })
-  // .createTable('project_resources')
+  .createTable('project_resources', table => {
+    // table.increments('project_resources_id')
+    // resources
+    table.integer('resource_id')
+      .unsigned()
+      .notNullable()
+      .references('resource_id')
+      .inTable('resources')
+    // products
+    table.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('project_id')
+      .inTable('projects')
+  })
 };
 
 exports.down = async function(knex) {
   await knex.schema
-  // .dropTableIfExists('project_resources')
+  .dropTableIfExists('project_resources')
   .dropTableIfExists('tasks')
   .dropTableIfExists('resources')
   .dropTableIfExists('projects')
