767a3804a1e7f5abc341430ce888ed87c2688a8f
Devon Osei <oseidevon@gmail.com>
2021-07-16 08:09:59 -0400

    initial setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
 // build your server here and require it from index.js
+const express = require('express')
+
+const server = express();
+
+server.use(express.json());
+
+server.use ('*', (res, req) => {
+  res.status(404).json({
+    message:'catch-all'
+  })
+})
+module.exports = server;
